941df2c7f472bf30da2f0e060719d61fcb5db2dc
Goryunofff <goryunov.ivan436@yandex.ru>
2024-10-05 20:55:01 +0300

    Обновление века (часть 1)

--- src/utils-constants/sort.js
+++ src/utils-constants/sort.js
@@ -1,11 +1,11 @@
 import dayjs from 'dayjs';
 
 const getTimeDifference = ({dateFrom, dateTo}) => dayjs(dateTo).diff(dayjs(dateFrom));
+const sortBy = {
+  Price: (pointA, pointB) => pointB.basePrice - pointA.basePrice,
+  Time: (pointA, pointB) => getTimeDifference(pointB) - getTimeDifference(pointA),
+  Day: (pointA, pointB) => dayjs(pointA.dateFrom) - dayjs(pointB.dateFrom),
+};
 
-const sortByPrice = (pointA, pointB) => pointB.basePrice - pointA.basePrice;
 
-const sortByTime = (pointA, pointB) => getTimeDifference(pointB) - getTimeDifference(pointA);
-
-const sortByDay = (pointA, pointB) => dayjs(pointA.dateFrom) - dayjs(pointB.dateFrom);
-
-export { sortByPrice, sortByTime, sortByDay };
+export { sortBy };
