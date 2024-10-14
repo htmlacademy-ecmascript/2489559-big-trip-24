08018a24071a0f95e5c0c6bc650a147a7e055ff0
Goryunofff <goryunov.ivan436@yandex.ru>
2024-10-14 17:16:14 +0300

    Добавит паттерн "Адаптер", Метод реализации responce-model, Удалит моковые данные

--- src/model/response-model.js
+++ src/model/response-model.js
@@ -1,12 +1,16 @@
-import { getRandomPoints } from '../mocks/points.js';
-import { mockDestinations } from '../mocks/destinations.js';
-import { mockOffers } from '../mocks/offers.js';
 import Observable from '../framework/observable.js';
 
 export default class PointsModel extends Observable {
-  #points = getRandomPoints;
-  #destinations = mockDestinations;
-  #offers = mockOffers;
+  #points = [];
+  #destinations = [];
+  #offers = [];
+
+  #pointsApiService = null;
+
+  constructor({pointsApiService}) {
+    super();
+    this.#pointsApiService = pointsApiService;
+  }
 
   get points() {
     return this.#points;
@@ -18,6 +22,23 @@ export default class PointsModel extends Observable {
 
   get offers() {
     return this.#offers;
+  }
+
+  async init() {
+    try {
+      const points = await this.#pointsApiService.points;
+      const offers = await this.#pointsApiService.offers;
+      const destinations = await this.#pointsApiService.destinations;
+
+      this.#points = points.map(this.adaptToClient);
+      this.#offers = offers.map((offer) => offer);
+      this.#destinations = destinations.map((destination) => destination);
+
+    } catch(err) {
+      this.#points = [];
+      this.#offers = [];
+      this.#destinations = [];
+    }
   }
 
   getDestinationsById(id) {
@@ -73,5 +94,21 @@ export default class PointsModel extends Observable {
     ];
 
     this._notify(updateType);
+  }
+
+  #adaptToClient(point) {
+    const adaptedPoint = {...point,
+      basePrice: point['base_price'],
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      isFavorite: point['is_favorite'],
+    };
+
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
   }
 }
